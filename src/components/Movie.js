7c710504168c45f0cbc5997082479889020c3423
bryanparmelee <bryan.parmelee@gmail.com>
2022-04-21 18:20:54 -0400

    Import Existing Repo
    Cloned repo from local machine to github

---
+++ src/components/Movie.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
+
+
+const Movie = ({ title, poster_path, overview, vote_average }) => (
+    <div className="movie">
+       
+        <img src={
+            poster_path ?
+            IMG_API + poster_path 
+            : "https://images.unsplash.com/photo-1518676590629-3dcbd9c5a5c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fG1vdmllfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" 
+            }
+            alt={title} />
+     
+
+       <div className="movie-overview">
+           <h2>{title}</h2>
+           <p>{overview}</p>
+           <p className="rating">{vote_average}/10</p>
+       </div>
+
+    </div>
+);
+
+export default Movie;
